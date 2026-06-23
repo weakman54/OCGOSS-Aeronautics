50ebff659710823a110077d4d1cb31e9e91c9022
Maya <maya@nyxi.sk>
2026-06-23 13:00:43 +0200

    change item stack limit to 64 for regular buckets

--- kubejs/startup_scripts/item_stack_limit.js
+++ kubejs/startup_scripts/item_stack_limit.js
@@ -4,6 +4,7 @@ ItemEvents.modification(event => {
     event.modify("minecraft:egg", item => { item.maxStackSize = 64 });
 
     //buckets
+    event.modify("minecraft:bucket", item => { item.maxStackSize = 64 });
     event.modify("minecraft:water_bucket", item => { item.maxStackSize = 64 });
     event.modify("minecraft:lava_bucket", item => { item.maxStackSize = 64 });
     event.modify("minecraft:milk_bucket", item => { item.maxStackSize = 64 });
