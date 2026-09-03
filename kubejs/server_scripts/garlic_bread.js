33f15b7a250f1cb19be19800817b84422961237d
MiaFoxcat <mia@miafoxcat.gay>
2026-09-03 20:28:51 +0200

    The gayening

---
+++ kubejs/server_scripts/garlic_bread.js
@@ -0,0 +1,5 @@
+ServerEvents.tags('item', event => {
+    event.add('ocgoss:garlic_bread', 'extradelight:garlic_bread');
+    event.add('ocgoss:garlic_bread', 'veggiesdelight:garlic_bread');
+    event.add('ocgoss:garlic_bread', 'pamhc2foodextended:garlicbreaditem');
+});
---
+++ kubejs/server_scripts/thigh_highs.js
@@ -0,0 +1,233 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:trans"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'estrogen:estrogen_pill',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:pride"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": '#minecraft:walls',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:pansexual"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'refurbished_furniture:frying_pan',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:lesbian"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'farmersdelight:cooked_bacon',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:genderqueer"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'estrogen:colon_three',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:genderqueer"}]'),
+        [
+            "ABC",
+        ],
+        {
+            "A": 'estrogen:estrogen_pill',
+            "B": 'estrogen:thigh_highs',
+            "C": 'estrogen:testosterone_powder'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:aromantic"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": '#c:cheese',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:non_binary"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'create:analog_lever',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:bisexual"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'extradelight:lemon_meringue_pie_slice',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:asexual"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": '#ocgoss:garlic_bread',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:vincian"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'minecraft:lightning_rod',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:genderfluid"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'estrogen:gender_change_potion',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:agender"}]'),
+        [
+            "B",
+        ],
+        {
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shaped(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:intersex"}]'),
+        [
+            "ABA",
+        ],
+        {
+            "A": 'minecraft:axolotl_bucket',
+            "B": 'estrogen:thigh_highs'
+        }
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:demisexual"}]'),
+        [
+            'estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:asexual"}]',
+            'estrogen:thigh_highs'
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polysexual"}]'),
+        [
+            'estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:bisexual"}]',
+            'estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:pansexual"}]',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+    event.shapeless(
+        Item.of('estrogen:thigh_highs[estrogen:thigh_high_style={style:"estrogen:polyamorous"}]'),
+        [
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+            'estrogen:thigh_highs',
+        ]
+    );
+});
